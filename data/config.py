3019196ff9fb5f1efa14bea32cf117816a7ac81e
juraevdev <juraevdevpy@gmail.com>
2025-08-20 18:44:48 +0500

    stuff

--- data/config.py
+++ data/config.py
@@ -1,10 +1,7 @@
 from environs import Env
 
-# environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = env.str("BOT_TOKEN")  
 ADMINS = env.list("ADMINS")  
-# IP = env.str("ip")  # Xosting ip manzili
--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,7 +2,7 @@ from aiogram import Router, types
 
 
 echo_router = Router()
-# Echo bot
+
 @echo_router.message()
 async def bot_echo(message: types.Message):
     await message.answer(message.text)
